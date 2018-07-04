3b6c6ded971bfc3e1265ce964bc272dd2cef1f46
kandmk <miku.rixia.0121@gmail.com>
2018-07-05 00:27:43 +0900

    main.py

--- python/main.py
+++ python/main.py
@@ -6,7 +6,11 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(u'<html><body><p>Hello,World!</p></body></html>')
+        self.response.write(u'<html><body><form action="/" method="POST">
+        1:<input 1="one" type="text" /><br />
+        2:<input 2="two" type="text" /><br />
+        <button type="submit">Enter</button>
+        </form></body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
