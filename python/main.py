cfa16100ad8f83feabca6f8f5a7463bdc9b358d2
kandmk <miku.rixia.0121@gmail.com>
2018-07-05 17:14:13 +0900

    main.py

--- python/main.py
+++ python/main.py
@@ -6,13 +6,15 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(u'<html><body><form action="/" method="POST">1:<input 1="one" type="text" /><br />2:<input 2="two" type="text" /><br /><button type="submit">Enter</button><p>patatokukasi</p></form></body></html>')
+        self.response.write(u'<html><body><form action="/">1:<input name="one" type="text" /><br />2:<input name="two" type="text" /><br /><button type="submit">Enter</button><p>%s</p></form></body></html>' % (self.patatokukasi()))
 
-    def patatokukasi:
+    def patatokukasi(self):
         ret = []
-            for num in range(len(one)):
-                ret.append(one[num])
-                ret.append(two[num])
+        one = self.request.get("one")
+        two = self.request.get("two")
+        for num in range(len(one)):
+            ret.append(one[num])
+            ret.append(two[num])
         ret = "".join(ret)
         return ret
         #self.response.write(u'<html><body><p>ret</p></body></html>')               
